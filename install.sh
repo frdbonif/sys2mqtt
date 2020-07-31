5a1ab2fd732fec3ff7e5cfce52b5976e5d5c0040
frdbonif <65509453+frdbonif@users.noreply.github.com>
2020-07-31 21:17:50 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 echo "Installer script for sys2mqtt v0.2.9"
+echo ""
+echo "Copying and enabling sys2mqtt service"
+
+cp sys2mqtt.service  /etc/systemd/system/sys2mqtt.service
+
+systemctl enable sys2mqtt.service
+systemctl start sys2mqtt.service
+
+echo ""
+echo "Service has now been enabled and started"
+echo "To change configuration, issue 'systemctl stop sys2mqtt' before editing"
+echo "the conf.py file."
